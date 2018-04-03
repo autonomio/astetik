ec18062c4b35da83264d32e292a5fbe773d6b06b
Mikko Kotila <mailme@mikkokotila.com>
2018-04-03 23:15:07 +0300

    deleted some files

--- test_script.py
+++ test_script.py
@@ -15,7 +15,7 @@ def strings():
 
 df['text'] = strings()
 
-ast.text(df, 'text', contains='NTECL', sort='asc', max_rows=5)
+ast.text(df, 'text', sort='asc', max_rows=5)
 ast.table(df[['A', 'B', 'C']], table_title='test')
 ast.ols(df, 'Survived', 'Age', 'Sex', 'Fare')
 ast.corr(df)
